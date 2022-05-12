d990d253ae91a2ce7cef05ec59e7a92c41092b71
dayann <dayanndeshayes@gmail.com>
2022-05-12 16:20:28 +0200

    correction de l'exo3

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -1,10 +1,21 @@
 package fr.m2i;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     //psvm pour creer pas fonction main plus rapidement !
     public static void main(String[] args){
 
+        Calculator myCalculator = new Calculator();
 
-
+        try {
+            myCalculator.start();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
