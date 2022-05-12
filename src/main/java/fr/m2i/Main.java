c61b2c84259c817fb58799b047f2fc5cc200d6b1
dayann <dayanndeshayes@gmail.com>
2022-05-12 14:06:42 +0200

    ajout de commentaire

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -7,11 +7,12 @@ public class Main {
     //psvm pour creer pas fonction main plus rapidement !
     public static void main(String[] args){
 
-    Integer num1 = null;
+    Integer num1 = null;//Integer est un wrapper
     Integer num2 = null;
     Integer operation = null;
     int result;
-    Integer i=null ;
+    int i= 0;//variable pour boucler dans mon while
+    int z = 0;//variable pour boucler dans mon while
 
     Scanner scanner = new Scanner(System.in);
     Calculator calcul = new Calculator();
@@ -23,33 +24,34 @@ public class Main {
             num2 = scanner.nextInt();
         }catch (InputMismatchException imp){
             System.out.println("Vous avez saisie un mauvais numéro");
-            scanner.nextLine();
+            scanner.nextLine();//Cette méthode peut lire l’entrée jusqu’à la fin de la ligne
         }
-/*        if (num1 == null){
-            scanner.nextLine();
-        }else {
-            try {
-                num2 = scanner.nextInt();
-            }catch (InputMismatchException imp){
-                System.out.println("Vous avez saisie un mauvais numéro");
-                scanner.nextLine();
-            }
-        }*/
     }
-    System.out.println("Selectionner un 1:addition,2:soustraction,3:division,4:multiplication");
-    while (i == 0){
-        operation = scanner.nextInt();
-        if (0<operation && operation>4){
-            System.out.println("Le numéro saisi est incorrect");
-            return;
-        }
-        i++;
-    }
-    scanner.close();
 
-    switch (operation){
+    while (i == 0){
+        try {
+            System.out.println("Selectionner un 1:addition,2:soustraction,3:division,4:multiplication");
+            operation = scanner.nextInt();
+            if (0<operation && operation>4){//Si operation est inferieur a 0 et suppérieur a 4
+                System.out.println("Le numéro saisi est incorrect");
+                continue;//on reprend direct a la condition de la boucle
+            }
+        }catch(InputMismatchException imp){
+            System.out.println("Vous avez saisie un mauvais numéro");
+            z=1;//Si l'utilisateur rentre des lettres j'utilise cette variable pour rentrer dans le if et faire un continue
+            scanner.nextLine();
+        }
+        if (z==1){
+            z=0;
+            continue;
+        }
+        i++;//J'utilise cette variable pour sortir de la boucle
+    }
+    scanner.close();//Je ferme mon scanner pour des soucis performance
+
+    switch (operation){//je fait un switch sur ma variable opération choisis par l'utilisateur pour lui renvoyer les données souhaiter
         case 1:
-            result = calcul.addition(num1,num2);
+            result = calcul.addition(num1,num2);//Je fait appelle à ma méthode dans la classe calculator
             System.out.println(String.format("%d + %d = %d",num1,num2,result));
             break;
         case 2:
@@ -67,20 +69,6 @@ public class Main {
         default:
             System.out.println("Oups il y a eu une erreur");
             break;
-    }
-
-
-/*    result = calcul.addition(num1,num2);
-    System.out.println(String.format("%d + %d = %d",num1,num2,result));
-
-    result = calcul.soustraction(num1,num2);
-    System.out.println(String.format("%d - %d = %d",num1,num2,result));
-
-    result = calcul.division(num1,num2);
-    System.out.println(String.format("%d / %d = %d",num1,num2,result));
-
-    result = calcul.multiplication(num1,num2);
-    System.out.println(String.format("%d * %d = %d",num1,num2,result));*/
-
+        }
     }
 }
