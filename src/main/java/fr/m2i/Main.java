2b138852d4737930e218b1334b0823818b9a0da4
dayann <dayanndeshayes@gmail.com>
2022-05-13 16:46:38 +0200

    corriger exercice Java 4

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -4,7 +4,17 @@ public class Main {
     //psvm pour creer pas fonction main plus rapidement !
     public static void main(String[] args){
 
+        Car car = new Car();
 
+        car.setSpeed(90);
+        car.setGasConsumption(5.5f);
+        car.setCurrentGas(50);
+
+        try {
+            car.drive();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 }
